3ce947cff0c3d87205b08d504dae8fe1ad012a40
Abdoullahi-dev <119604973+Abdoullahi-dev@users.noreply.github.com>
2024-09-06 03:25:16 +0300

    Fixed it. Now you can have capitalization.

--- Caesar.py
+++ Caesar.py
@@ -1,8 +1,16 @@
-list = [
+lower_list = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
     "y", "z"
 ]
+
+upper_list = []
+
+for i in lower_list:
+    upper_list.append(i.upper())
+
+print(upper_list)
+print(lower_list)
 
 
 def shift(text, displacement):
@@ -12,8 +20,14 @@ def shift(text, displacement):
         text_list.append(i)
     for i in text_list:
         if i != " ":
-            shift_amount = (list.index(i) + displacement) % len(list)
-            text_list[position] = list[shift_amount]
+            if i.isupper():
+                shift_amount = (upper_list.index(
+                    i) + displacement) % len(upper_list)
+                text_list[position] = upper_list[shift_amount]
+            else:
+                shift_amount = (lower_list.index(
+                    i) + displacement) % len(lower_list)
+                text_list[position] = lower_list[shift_amount]
         position += 1
     return "".join(text_list)
 
@@ -25,13 +39,19 @@ def unshift(text, displacement):
         text_list.append(i)
     for i in text_list:
         if i != " ":
-            shift_amount = (list.index(i) - displacement) % len(list)
-            text_list[position] = list[shift_amount]
+            if i.isupper():
+                shift_amount = (upper_list.index(
+                    i) - displacement) % len(upper_list)
+                text_list[position] = upper_list[shift_amount]
+            else:
+                shift_amount = (lower_list.index(
+                    i) - displacement) % len(lower_list)
+                text_list[position] = lower_list[shift_amount]
         position += 1
     return "".join(text_list)
 
 
-num = 31
-text = "jon john"
+num = 1
+text = "aBc DeF"
 print(shift(text, num))
 print(unshift(shift(text, num), num))
